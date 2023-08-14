79b60fcefdc6389e26bde3757bf900c2d6183336
Esxotic <wayanjerman63@gmail.com>
2023-08-14 22:44:41 +0800

    Membuat template Login

--- resources/views/login/start.blade.php
+++ resources/views/login/start.blade.php
@@ -1,36 +1,15 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Web-Movie-PRG009</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+@extends('layouts.login')
 
-    {{-- my css --}}
-    <link rel="stylesheet" href="/css/style.css">
-
-    {{-- google font --}}
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@800&family=Moul&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <div class="container">
-        <div class="row text-center">
-            <div class="col-md-6 pt-5 section1">
-                <h1 style="font-family:Moul"><span class="text-purple">mov</span>ie+</h1>
-                <div class="d-grid gap-3 pt-5">
-                  <a href="" class="btn btn-light">SIGN IN</a>
-                  <a href="" class="btn btn-light"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16"> <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/></svg> CONTINUE WITH GOOGLE</a>
-                </div>
-                <p class="tagline">Watch your favourite movie or series on only one platform. You can watch it anytime and anywhere</p>
-            </div>
-            <div class="col-md-6 section2">
-                <img src="/img/14.jpg" alt="herry potter">
-            </div>
-        </div>
+@section('container')
+<div class="col-md-6 pt-5 section1">
+    <h1 style="font-family:Moul"><span class="text-purple">mov</span>ie+</h1>
+    <div class="d-grid gap-3 pt-5">
+      <a href="" class="btn btn-light">SIGN IN</a>
+      <a href="" class="btn btn-light"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16"> <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/></svg> CONTINUE WITH GOOGLE</a>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-  </body>
-</html>
+    <p class="tagline">Watch your favourite movie or series on only one platform. You can watch it anytime and anywhere</p>
+</div>
+<div class="col-md-6 section2">
+    <img src="/img/14.jpg" alt="herry potter">
+</div>
+@endsection
