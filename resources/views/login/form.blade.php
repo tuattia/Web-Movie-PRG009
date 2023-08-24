cd8e638f6cb5ab9f05d2d84271c280183d32a801
Esxotic <wayanjerman63@gmail.com>
2023-08-24 16:32:11 +0800

    Memperbaiki alert pada view form

--- resources/views/login/form.blade.php
+++ resources/views/login/form.blade.php
@@ -2,13 +2,6 @@
 
 @section('container')
 <div class="col-md-6 mt-5">
-    @if (session()->has('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('success') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    @endif
-
     @if (session()->has('loginError'))
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
             {{ session('loginError') }}
